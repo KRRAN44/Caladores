32b92d1288f30452ac47ff0bfa693c726540afe0
KRRAN44 <7hg4vht7v4@privaterelay.appleid.com>
2026-08-06 17:35:22 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+  
+    const input = document.getElementById('web');
+    input.addEventListener('input', function() {
+      if (this.value === "otp") {
+        window.location.href = "https://krran44.github.io/OTP-SEARCH-COD-MARINE/?v=2";
+      }
+      if (this.value === "tm") {
+        window.location.href = "https://t.me/addlist/P99xl6ggoQAyNDFh";
+      }
+      if (this.value === "Bottm") {
+        window.location.href = "https://t.me/TricksMastarNumberFile2_bot";
+      }
+    });
+   
