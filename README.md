c7ac4df1ac130113c7e9b88a2f6c4d50fd7a8e1d
KRRAN44 <7hg4vht7v4@privaterelay.appleid.com>
2026-08-08 09:09:34 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# Caladores
+🜲⸸ 𝐂𝕒͢ℓ𝕒d𝕠𝗿͒𝒆sꪶ𑁮ིུ࿆𑁬ꫂ ⸸🜲᭄
+
+CODS:
+otp === OTP search, buscador de archivos
+tm === archivos y otp recive de trickstar master
+Bottm === bot de trickstar master, archivos grandes
+seven === carpeta archivos de seven1tel
+Botseven === otp recive de seven1tel
---
+++ script.js
@@ -0,0 +1,20 @@
+  
+    const input = document.getElementById('web');
+    input.addEventListener('input', function() {
+      if (this.value === "otp") {
+        window.location.href = "https://krran44.github.io/OTP-SEARCH-COD-MARINE/?v=2";
+      }
+      if (this.value === "tm") {
+        window.location.href = "https://t.me/addlist/P99xl6ggoQAyNDFh";
+      }
+      if (this.value === "Bottm") {
+        window.location.href = "https://t.me/TricksMastarNumberFile2_bot";
+      }
+      if (this.value === "seven") {
+        window.location.href = "https://t.me/addlist/19QGJF24iEk4NmRh";
+      }
+      if (this.value === "Botseven") {
+        window.location.href = "https://t.me/+mHJ9ZOyBKIdjYjJl";
+      }
+    });
+   
